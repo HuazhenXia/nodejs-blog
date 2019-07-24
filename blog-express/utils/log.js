9b5becb2a38c2e8822556e89ff5f32812dc10524
huazhenxia <imxhz@foxmail.com>
2019-07-23 23:42:28 -0700

    init express blog

---
+++ blog-express/utils/log.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+
+function writeLog(writeStream, log) {
+    writeStream.write(log + '\n')
+}
+
+// write Stream
+function createWriteStream(fileName) {
+    const fullFileName = path.join(__dirname, '../', '../', 'logs', fileName)
+    const writeStream = fs.createWriteStream(fullFileName, {
+        flags: 'a'
+    })
+    return writeStream
+}
+
+// access log
+const accessWriteStream = createWriteStream('access.log')
+function access(log) {
+    writeLog(accessWriteStream, log)
+}
+
+module.exports = {
+    access
+}
